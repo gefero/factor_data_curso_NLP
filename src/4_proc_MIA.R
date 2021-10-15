fb60a619ba17b5b0556cff82a21d7abcfb4aef48
German Rosati <german.rosati@gmail.com>
2021-10-15 16:53:39 -0300

    Agrego diapos Clase 1 y 2

--- src/4_proc_MIA.R
+++ src/4_proc_MIA.R
@@ -10,5 +10,7 @@ df %>%
 
 
 df %>%
-        filter(autor %in% c('rosa luxemburgo', 'lenin')) %>%
+        filter(autor %in% c('Rosa Luxemburgo', 'lenin')) %>%
+        group_by(autor) %>%
+        sample_n(46) %>%
         write_csv('./clase1/data/lenin_luxemburgo.csv')
